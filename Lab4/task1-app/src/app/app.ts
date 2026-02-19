764c8921e60661d9914bc903bc9df6008795e852
Synbat07 <zajlaubaevasynbat@gmail.com>
2026-02-19 10:15:02 +0500

    output

--- Lab4/task1-app/src/app/app.ts
+++ Lab4/task1-app/src/app/app.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
+import { Child } from './child';
 
 @Component({
   selector: 'app-root',
-  template: `<app-user name="Simran"></app-user>`,
+  imports: [Child],
+  template: `
+    <app-child (addItemEvent)="addItem($event)" />
+    <p>🐢 all the way down {{ items.length }}</p>
+  `,
 })
-export class App {}
+export class App {
+
+  items = new Array<string>();
+
+  addItem(item: string) {
+    this.items.push(item);
+  }
+}
