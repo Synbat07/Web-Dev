f69a079fa44dbeb6c74f8cdd90e8aacec184a059
Synbat07 <zajlaubaevasynbat@gmail.com>
2026-02-19 10:55:35 +0500

    Create a custom pipe

--- Lab4/task1-app/src/app/app.ts
+++ Lab4/task1-app/src/app/app.ts
@@ -1,20 +1,11 @@
 import { Component } from '@angular/core';
-import { DecimalPipe, DatePipe, CurrencyPipe } from '@angular/common';
+import { ReversePipe } from './reverse.pipe';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [DecimalPipe, DatePipe, CurrencyPipe],
-  template: `
-    <ul>
-      <li>Number with "decimal" {{ num | number:'3.2-2' }}</li>
-      <li>Date with "date" {{ birthday | date:'medium' }}</li>
-      <li>Currency with "currency" {{ cost | currency }}</li>
-    </ul>
-  `,
+  template: `Reverse Machine: {{ word | reverse }}`,
+  imports: [ReversePipe],
 })
 export class App {
-  num = 103.1234;
-  birthday = new Date('2023-04-02');
-  cost = 4560.34;
+  word = 'You are a champion';
 }
